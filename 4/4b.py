e7dc25e2c04c3c5c7836e05fd8fb184e93c78de0
bobert <bobertstark@users.noreply.github.com>
2025-12-04 11:33:17 -0500

    day 4

---
+++ 4/4b.py
@@ -0,0 +1,60 @@
+def main(grid):
+    access_tot = 0
+    access_iter = 0
+
+    grid, access_iter = access_grid(grid)
+    while access_iter:
+        access_tot += access_iter
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if grid[row][col] == 'x':
+                    grid[row][col] = '.'
+        grid, access_iter = access_grid(grid)
+
+    # for row in grid:
+    #     print(''.join(row))
+    print(f'Accessible: {access_tot}')
+
+def access_grid(grid):
+    MAX_ROW = len(grid) - 1
+    MAX_COL = len(grid[0]) - 1
+
+    def few_neighbors(grid, row, col) -> bool:
+        # . . .
+        # . @ .
+        # . . .
+        count = 0
+
+        pots = [
+            (row+1, col), (row+1, col+1), (row+1, col-1),
+            (row, col+1), (row, col-1),
+            (row-1, col), (row-1, col+1), (row-1, col-1),
+        ]
+        for pr, pc in pots:
+            if pr < 0 or pr > MAX_ROW or pc < 0 or pc > MAX_COL:
+                continue
+            val = grid[pr][pc]
+            if val in '@x':
+                count += 1
+            if count >= 4:
+                return False
+        return True
+
+    access_cnt = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] != '@':
+                continue
+            if few_neighbors(grid, row, col):
+                # assign as accessible
+                grid[row][col] = 'x'
+                access_cnt += 1
+
+    return grid, access_cnt
+
+
+if __name__ == '__main__':
+    import sys
+    with open(sys.argv[-1]) as f:
+        data = [list(fline.strip()) for fline in f.readlines()]
+    main(data)
